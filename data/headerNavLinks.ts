b059fa3c8e4e9576d8e393d675d4835feb621520
Mach-Mach <mch4681254@gmail.com>
2024-11-14 12:13:29 -0700

    initial commit

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,10 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/blog', title: 'Articles' },
+  { href: '/blog', title: 'News' },
+  { href: '/tags', title: 'Tags' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/about', title: 'About' },
+]
+
+export default headerNavLinks
