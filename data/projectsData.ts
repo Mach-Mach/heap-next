319c45438f7f192631d1493c2e4819b578a42e91
Maryna Chernyshevich <133984341+Mach-Mach@users.noreply.github.com>
2024-11-14 22:45:57 +0000

    upd

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,14 +7,6 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
     title: 'ML Fundamentals',
     description: `Welcome to ML Fundamentals! Here, we break down the basics of Machine Learning in a way that’s easy to follow and ready to use. Get a handle on core ideas, from algorithms to model building, so you can confidently dive into the world of ML. Perfect for anyone looking to get started or brush up on the essentials.`,
     imgSrc: '/static/images/time-machine.jpg',
