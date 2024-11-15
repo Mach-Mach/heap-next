bd52e23c839aa6444a5a3c6566372b5cc6cca220
Mach-Mach <mch4681254@gmail.com>
2024-11-15 14:12:02 -0700

    upd

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -41,7 +41,7 @@ const siteMetadata = {
     //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     // },
     googleAnalytics: {
-      googleAnalyticsId: 'G-PTCTM65ZWQ', // e.g. G-XXXXXXX
+      googleAnalyticsId: '', // e.g. G-XXXXXXX
     },
   },
   newsletter: {
